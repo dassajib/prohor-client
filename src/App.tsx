663c6ebfd61f8238af689ebae0a9327b971ef8ef
dassajib <sajibdas.sd@gmail.com>
2025-06-19 02:15:45 +0600

    1.routing implemented 2.registration form design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+  return <AppRoutes />
 }
 
 export default App
